4ae71ff6f8f6a631607e51dc0d3b94817ba45f07
DisneyTurek <dturek2@huskers.unl.edu>
2023-08-24 10:01:35 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Disney Turek
+ *         dturek2@unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
